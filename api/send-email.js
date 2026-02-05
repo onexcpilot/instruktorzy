90da8485ca6c3e21deb25e1a70d5ab00eb1e3bbe
Sierra Zulu Dev <onexcpilot@gmail.com>
2026-02-05 16:28:18 +0100

    fix: add accessToken to EmailJS API call for server-side

--- api/send-email.js
+++ api/send-email.js
@@ -35,6 +35,7 @@ export default async function handler(req, res) {
                 service_id: process.env.EMAILJS_SERVICE_ID,
                 template_id: process.env.EMAILJS_TEMPLATE_ID,
                 user_id: process.env.EMAILJS_PUBLIC_KEY,
+                accessToken: process.env.EMAILJS_PRIVATE_KEY, // Dodano klucz prywatny dla backendu
                 template_params: {
                     to_email,
                     password,
